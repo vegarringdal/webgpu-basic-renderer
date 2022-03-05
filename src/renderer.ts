4efdc8008a34792af085a50b076978c81a7adaa7
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-03-05 16:57:06 +0100

    update code to remove warnings

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,107 +1,118 @@
-import { Scene } from './scene';
-import { Camera } from './camera';
+import { Scene } from "./scene";
+import { Camera } from "./camera";
 
 export var device: GPUDevice;
 
 export class WebGpuRenderer {
+  readonly swapChainFormat = "bgra8unorm";
+  private initSuccess: boolean = false;
+  private renderPassDescriptor: GPURenderPassDescriptor;
+  private context: GPUCanvasContext;
+  private presentationFormat: GPUTextureFormat;
+  private presentationSize: number[];
 
-    readonly swapChainFormat = 'bgra8unorm';
-    private initSuccess: boolean = false;
-    private renderPassDescriptor: GPURenderPassDescriptor;
-    private context: GPUCanvasContext;
-    private presentationFormat: GPUTextureFormat;
-    private presentationSize: number[];
+  constructor() {}
 
-    constructor() { }
-
-    public async init(canvas: HTMLCanvasElement): Promise<boolean> {
-        if (!canvas) {
-            console.log('missing canvas!')
-            return false;
-        }
-
-        const adapter = await navigator.gpu.requestAdapter();
-        device = await adapter.requestDevice();
-
-        if (!device) {
-            console.log('found no gpu device!')
-            return false;
-        }
-
-        this.context = canvas.getContext('webgpu');
-
-        this.presentationFormat = this.context.getPreferredFormat(adapter);
-        this.presentationSize = [
-            canvas.clientWidth * devicePixelRatio,
-            canvas.clientHeight  * devicePixelRatio,
-        ];
-
-        this.context.configure({
-            device,
-            format: this.presentationFormat,
-            size: this.presentationSize,
-        });
-        
-
-        const depthTextureView = this.depthTextureView(canvas);
-        this.renderPassDescriptor = {
-            colorAttachments: [
-                {
-                    // attachment is acquired and set in render loop.
-                    view: undefined,
-                    loadValue: { r: 0.5, g: 0.5, b: 0.5, a: 1.0 },
-                } as GPURenderPassColorAttachment,
-            ],
-            depthStencilAttachment: {
-                view: depthTextureView,
-
-                depthLoadValue: 1.0,
-                depthStoreOp: 'store',
-                stencilLoadValue: 0,
-                stencilStoreOp: 'store',
-            } as GPURenderPassDepthStencilAttachment,
-        };
-
-        return this.initSuccess = true;
+  public async init(canvas: HTMLCanvasElement): Promise<boolean> {
+    if (!canvas) {
+      console.log("missing canvas!");
+      return false;
     }
 
-    public update(canvas: HTMLCanvasElement) {
-        if (!this.initSuccess) {
-            return;
-        }
+    const adapter = await navigator.gpu.requestAdapter();
+    device = await adapter.requestDevice();
 
-        this.updateRenderPassDescriptor(canvas);
+    if (!device) {
+      console.log("found no gpu device!");
+      return false;
     }
 
-    public frame(camera: Camera, scene: Scene) {
-        if (!this.initSuccess) {
-            return;
-        }
+    this.context = canvas.getContext("webgpu");
 
-        (this.renderPassDescriptor.colorAttachments as [GPURenderPassColorAttachment])[0].view = this.context
-            .getCurrentTexture()
-            .createView();
+    this.presentationFormat = this.context.getPreferredFormat(adapter);
+    this.presentationSize = [
+      canvas.clientWidth * devicePixelRatio,
+      canvas.clientHeight * devicePixelRatio,
+    ];
 
-        const commandEncoder = device.createCommandEncoder();
-        const passEncoder = commandEncoder.beginRenderPass(this.renderPassDescriptor);
+    this.context.configure({
+      device,
+      format: this.presentationFormat,
+      size: this.presentationSize,
+    });
 
-        for (let object of scene.getObjects()) {
-            object.draw(passEncoder, device, camera)
-        }
+    const depthTextureView = this.depthTextureView(canvas);
+    this.renderPassDescriptor = {
+      colorAttachments: [
+        {
+          // attachment is acquired and set in render loop.
+          view: undefined,
+          loadOp: "clear",
+          clearValue: { r: 0.5, g: 0.5, b: 0.5, a: 1.0 },
+          storeOp: "store",
+        } as GPURenderPassColorAttachment,
+      ],
+      depthStencilAttachment: {
+        view: depthTextureView,
 
-        passEncoder.endPass();
-        device.queue.submit([commandEncoder.finish()]);
+        depthLoadOp: "clear",
+        depthClearValue: 1.0,
+        depthStoreOp: "store",
+        stencilClearValue: 0,
+        stencilLoadOp: "load",
+        stencilStoreOp: "store",
+      } as GPURenderPassDepthStencilAttachment,
+    };
+
+    return (this.initSuccess = true);
+  }
+
+  public update(canvas: HTMLCanvasElement) {
+    if (!this.initSuccess) {
+      return;
     }
 
-    private depthTextureView(canvas: HTMLCanvasElement) {
-        return device.createTexture({
-            size: this.presentationSize,
-            format: 'depth24plus-stencil8',
-            usage: GPUTextureUsage.RENDER_ATTACHMENT,
-        }).createView();
+    this.updateRenderPassDescriptor(canvas);
+  }
+
+  public frame(camera: Camera, scene: Scene) {
+    if (!this.initSuccess) {
+      return;
     }
 
-    private updateRenderPassDescriptor(canvas: HTMLCanvasElement) {
-        (this.renderPassDescriptor.depthStencilAttachment as GPURenderPassDepthStencilAttachment).view = this.depthTextureView(canvas);
+    (
+      this.renderPassDescriptor.colorAttachments as [
+        GPURenderPassColorAttachment
+      ]
+    )[0].view = this.context.getCurrentTexture().createView();
+
+    const commandEncoder = device.createCommandEncoder();
+    const passEncoder = commandEncoder.beginRenderPass(
+      this.renderPassDescriptor
+    );
+
+    for (let object of scene.getObjects()) {
+      object.draw(passEncoder, device, camera);
     }
+
+    passEncoder.end();
+    device.queue.submit([commandEncoder.finish()]);
+  }
+
+  private depthTextureView(canvas: HTMLCanvasElement) {
+    return device
+      .createTexture({
+        size: this.presentationSize,
+        format: "depth24plus-stencil8",
+        usage: GPUTextureUsage.RENDER_ATTACHMENT,
+      })
+      .createView();
+  }
+
+  private updateRenderPassDescriptor(canvas: HTMLCanvasElement) {
+    (
+      this.renderPassDescriptor
+        .depthStencilAttachment as GPURenderPassDepthStencilAttachment
+    ).view = this.depthTextureView(canvas);
+  }
 }
